e343b751a6ae5c81d8181b45ac82a1e2e1e3cc6c
Nicholas Roberto <njr444@gmail.com>
2020-01-08 22:08:51 -0800

    added event listener and button for changing content written to nametag project.

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
-console.log("Word");
+ const helloText =
+ document.getElementById('the-name');
 
+ const myInput = document.getElementById('myInput');
+
+ const button = document.getElementById('submit');
+
+ const changeName = () => {
+     helloText.textContent = myInput.value;
+ };
+
+button.addEventListener('click', changeName);
+
+
+//const myName = prompt('What is your name?');
+
+//helloText.textContent = myName;
+
+//helloText.textContent = "greetings";
+//helloText.style.color = 'green';
+
+
+/*for (let i=0; i < 15; i ++){
+    if (i < 10){
+        console.log('fizz');
+    } else if (i >= 10 ) {
+        console.log('buzz');
+    }
+} */
